46cff92b7846072c3b655ccbd7b2e27aee3431c9
Michael Blackmon <miblackmon@davidson.edu>
2020-04-26 21:35:21 -0400

    add support for auth_state

--- duoauthenticator/duoauthenticator.py
+++ duoauthenticator/duoauthenticator.py
@@ -1,8 +1,11 @@
+import base64
+from jupyterhub.crypto import encrypt, decrypt
 from jupyterhub.auth import Authenticator, PAMAuthenticator
 from jupyterhub.handlers import LoginHandler
 from tornado import gen
 from traitlets import Unicode, Type, Instance, default
 import duo_web
+import json
 
 class DuoHandler(LoginHandler):
     """Duo Two-Factor Handler"""
@@ -22,21 +25,35 @@ class DuoHandler(LoginHandler):
         if sig_response:
             # Duo signed response present, do secondary auth
             yield LoginHandler.post(self)
+            
         else:
             # no sig_response, do primary auth and generate the request
-            username = yield self.authenticator.do_primary_auth(self,data)
-            if username:
-                sig_request = duo_web.sign_request(self.authenticator.ikey,
-                    self.authenticator.skey, self.authenticator.akey, username)
-                html = self.render_template('duo.html',
-                    host=self.authenticator.apihost,
-                    sig_request=sig_request,
-                    custom_html = self.authenticator.duo_custom_html)
+            auth = yield self.authenticator.do_primary_auth(self,data)
+            if auth:
+                sig_request = duo_web.sign_request(
+                    self.authenticator.ikey,
+                    self.authenticator.skey,
+                    self.authenticator.akey,
+                    auth.get('name'))
+
+                html = self.render_template(
+                    'duo.html',
+                    host = self.authenticator.apihost,
+                    sig_request = sig_request,
+                    auth_cache = base64.binascii.b2a_base64(
+                        json.dumps(
+                            auth
+                        ).encode('ascii')
+                    ).decode('ascii'),
+                    custom_html = self.authenticator.duo_custom_html
+                )
+                
                 self.finish(html)
+                
             else:
                 html = self._render(
                     login_error='Invalid username or password',
-                    username=username,
+                    username=data.get('username'),
                 )
                 self.finish(html)
 
@@ -107,14 +124,34 @@ class DuoAuthenticator(Authenticator):
 
         Return None otherwise.
         """
+        self.log.debug("DUO-DATA: %s", repr(data)[:100])
 
+        auth = json.loads(
+            base64.binascii.a2b_base64(
+                    data['auth_cache'].encode('ascii')
+                ).decode('ascii')
+            )
+    
+        from pprint import pformat
+        self.log.debug(pformat(auth))
+        
         sig_response = data['sig_response']
-        authenticated_username = duo_web.verify_response(self.ikey,\
-            self.skey, self.akey, sig_response)
-        if authenticated_username:
+        authenticated_username = \
+            duo_web.verify_response(
+                self.ikey,
+                self.skey,
+                self.akey,
+                sig_response)
+
+        if authenticated_username == auth.get('name'):
             self.log.debug("Duo Authentication succeeded for user '%s'", \
-                authenticated_username)
-            return authenticated_username
+                           auth.get('name'))
+            
+            # XXX: FUCK Duo.... This required black-magic and blood-sacrifice to work
+            #  -- Michael B.
+            self.log.debug(".... %s:", repr(auth)[:100])
+            return auth
+        
         else:
             self.log.warning("Duo Authentication failed for user '%s'", username)
             return None
@@ -125,8 +162,8 @@ class DuoAuthenticator(Authenticator):
 
         Return None otherwise.
         """
-        primary_username = yield self.primary_authenticator.authenticate(handler, data)
-        if primary_username:
-            return primary_username
+        primary_auth = yield self.primary_authenticator.authenticate(handler, data)
+        if primary_auth:
+            return primary_auth
         else:
             return None
